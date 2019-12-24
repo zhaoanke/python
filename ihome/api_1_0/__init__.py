8e0822ff6e9cb13ede11179f8ab0d64c5e5dcc15
zhaoanke <1259226841@qq.com>
2019-12-24 14:47:21 +0800

    update manage add models，views log

---
+++ ihome/api_1_0/__init__.py
@@ -0,0 +1,8 @@
+# coding:utf-8
+from flask import Blueprint
+
+# 创建蓝盘对象
+api = Blueprint("api_1_0", __name__)
+
+# 导入蓝图的视频
+from . import index
--- manage.py
+++ manage.py
@@ -1,30 +1,13 @@
 # coding:utf-8
-from flask_session import Session
-from flask_wtf import CSRFProtect
-
-import redis
-from ihome import create_app
-
+from ihome import create_app, db
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 # 创建flask 的应用对象
 app = create_app("develop")
 
-
-
-# 创建redis连接对象
-redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
-
-# 利用flask-session，将session数据保存到redis中
-Session(app)
-
-# 为flask补充csrf防护
-CSRFProtect(app)
-
-
-@app.route("/index")
-def index():
-    session[] =
-    session.get()
-    return "index page"
+manager = Manager(app)
+Migrate(app, db)
+manager.add_command("db", MigrateCommand)
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
